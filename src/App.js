06af9890066cdf13173d1d42bee514bef0f5219b
Hamdan Salih <hamdansalihpc@gmail.com>
2023-01-29 12:42:42 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import Example from './Components/Example';
-
 
 function App() {
+  const [BestScore, setBestScore] = useState(0);
   const [Score, setScore] = useState(0);
   const [Selection, setSelection] = useState('');
-
   const [Cards, setCards] = useState([
-    {id: 0 , name: 'Harry Potter'},
-    {id: 1 , name: 'Hermonie Granger'},
-    {id: 2 , name: 'Ron Weasly'},
-    {id: 3 , name: 'Voldemort'},
-    {id: 4 , name: 'Freddo Baggins'},
-    {id: 5 , name: 'Bilbo Baggins'},
-    {id: 6 , name: 'Grandalf'},
-    {id: 7 , name: 'Albus Dumbledore'},
-    {id: 8 , name: 'Sauron'},
-    {id: 9 , name: 'Aragon'},
-    {id: 10 , name: 'Morgoth'},
-    {id: 11 , name: 'Severus Snape'},
+    {id: 0 , name: 'Hogwarts', img : 'images/Hogwarts.jpg'},
+    {id: 1 , name: 'Diagon Alley', img : 'images/Diagon-Alley.jpg'},
+    {id: 2 , name: 'Hogsmeade' , img : 'images/Hogsmeade.jpg'},
+    {id: 3 , name: 'Mordor', img : 'images/Mordor.jpg'},
+    {id: 4 , name: 'Edoras', img : 'images/Edoras.jpg'},
+    {id: 5 , name: 'Minas Tirith', img : 'images/Minas-Tirith.jpg'},
+    {id: 6 , name: 'Misty Mountains', img : 'images/Misty-Moutains.jpg'},
+    {id: 7 , name: 'Asguard', img : 'images/Asguard.jpg'},
+    {id: 8 , name: 'Gotham City', img : 'images/Gotham-City.jpg'},
+    {id: 9 , name: 'Planet Titan', img : 'images/Planet-Titan.jpg'},
+    {id: 10 , name: 'Winterfell', img : 'images/Winterfell.jpg'},
+    {id: 11 , name: 'Dragonstone', img : 'images/Dragonstone.jpg'},
 
   ]);
+
+  useEffect(() => {
+    console.log(Selection)
+    if(BestScore < Score){
+      setBestScore(Score)
+    }
+  });
+
+  function contains(a, obj) {
+    if(a){
+      for (var i = 0; i < a.length; i++) {
+          if (a[i] === obj) {
+              return true;
+          }
+      }
+    }
+    return false;
+  } 
+
 
   function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -36,17 +53,36 @@ function App() {
     return array;
   }
 
+  const handleChange = (item)=>{
+    let check = contains(Selection , item)
+    console.log(check)
+    setSelection(() => {
+        if(check){
+          setScore(0)
+          return []
+        }
+        else{
+          setScore(Score + 1)
+          return [...Selection, item]
+        }
+    })
+  }
+
+
   return (
     <div className="App">
-
+      <div className='score-board'>
+        <h3>Score : {Score}</h3>
+        <h3>High Score : {BestScore}</h3>
+      </div>
       <div className='display-card'>{Cards.map((character)=>{
         return (
           <div className='card' onClick={()=>{
-            setCards([...shuffle(Cards)])
-            setSelection([...Selection,character.id])
-            console.log(Selection)
-
+            setCards([...shuffle(Cards)]);
+            handleChange(character.id)
+            console.log(character.img)
           }}>
+            <img src={character.img} alt={character.name} srcset="" />
             <h2>{character.name}</h2>
           </div>
         )
